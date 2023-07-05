fa725de4c011251570651acc7008e8ceae9f8c1d
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-05 15:37:16 -0400

    Add component to about path

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/app/components/main/Navbar";
+
+export default function Page() {
+    return (
+        <>
+            <Navbar />
+            <h1>About</h1>
+        </>
+    )
+}
