f0059d8dfa1ad0f677cc82c97a48b1e836c7ddd5
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-05 15:36:58 -0400

    Add component to products path

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/app/components/main/Navbar";
+
+export default function Page() {
+    return (
+        <>
+            <Navbar />
+            <h1>Product</h1>
+        </>
+    )
+}
