5e887fc5ecd54001c21c5dc2eb20e3053a15c5b8
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-07 10:14:30 -0400

    Remove Navbar component in carts/[cartId] path

--- src/app/carts/[cartId]/page.tsx
+++ src/app/carts/[cartId]/page.tsx
@@ -1,9 +1,6 @@
-import Navbar from "@/app/components/main/Navbar";
-
 export default function Page({ params }: { params: { cartId: string } }) {
     return (
         <>
-            <Navbar />
             <h1>Cart {params.cartId}</h1>
         </>
     )
