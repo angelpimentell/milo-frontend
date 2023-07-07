15fbd9d8144360b511b0478dc1b680ef87a8218d
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-07 10:14:59 -0400

    Remove Navbar component in about path

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,6 @@
-import Navbar from "@/app/components/main/Navbar";
-
 export default function Page() {
     return (
         <>
-            <Navbar />
             <h1>About</h1>
         </>
     )
