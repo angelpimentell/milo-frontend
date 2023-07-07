da72002445a55cd31b2a4e89a80548931c7938f0
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-07 10:14:47 -0400

    Remove Navbar component in carts path

--- src/app/carts/page.tsx
+++ src/app/carts/page.tsx
@@ -1,10 +1,8 @@
-import Navbar from "@/app/components/main/Navbar";
 import CartList from "../components/CartList";
 
 export default function Page() {
     return (
         <>
-            <Navbar />
             <CartList />
         </>
     )
