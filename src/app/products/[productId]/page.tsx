2cec4347108cd7da80d91468a983193e1eaad65f
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-07 10:11:33 -0400

    Remove Navbar component in products/[productId] path

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,6 @@
-import Navbar from "@/app/components/main/Navbar";
-
 export default function Page({ params }: { params: { productId: string } }) {
     return (
         <>
-            <Navbar />
             <h1>Product {params.productId}</h1>
         </>
     )
