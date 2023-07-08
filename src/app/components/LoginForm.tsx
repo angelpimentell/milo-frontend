caa89ce46e29cd68c8b67c93292f18c899996b59
Angel Pimentel <angelpimentelcontact@gmail.com>
2023-07-08 10:50:16 -0400

    Change register and login form colors

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -4,19 +4,19 @@ export default function LoginForm() {
     return (
         <>
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
 
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-black md:text-2xl dark:text-black">
                             Sign in to your account
                         </h1>
                         <form className="space-y-4 md:space-y-6" action="#">
                             <div>
-                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Your email</label>
                                 <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
                             </div>
                             <div>
-                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-black">Password</label>
                                 <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                             </div>
                             <div className="flex items-center justify-between">
@@ -25,13 +25,13 @@ export default function LoginForm() {
                                         <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
                                     </div>
                                     <div className="ml-3 text-sm">
-                                        <label className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                        <label className="text-gray-500 dark:text-black">Remember me</label>
                                     </div>
                                 </div>
                                 {/* <a href="#" className="text-sm font-medium text-primary-600 hover:underline text-slate-50">Forgot password?</a> */}
                             </div>
                             <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-800 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                            <p className="text-sm font-light text-gray-500 dark:text-black">
                                 Don’t have an account yet? <Link href="/register" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
                             </p>
                         </form>
